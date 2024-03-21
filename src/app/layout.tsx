269d11fe67075c33513c10fe9a75bc2e2a674dfd
LamTien2003 <ltt209514@gmail.com>
2024-03-21 20:00:04 +0700

    add facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,12 +36,8 @@ export default function RootLayout({
     <html lang="en">
       <Script
         id="facebook-pixel"
-        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-                    <!-- Meta Pixel Code -->
-                      <script>
-                      !function(f,b,e,v,n,t,s)
+          __html: `!function(f,b,e,v,n,t,s)
                       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
                       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -51,14 +47,18 @@ export default function RootLayout({
                       'https://connect.facebook.net/en_US/fbevents.js');
                       fbq('init', '611716307821381');
                       fbq('track', 'PageView');
-                      </script>
-                      <noscript><img height="1" width="1" style="display:none"
-                      src="https://www.facebook.com/tr?id=611716307821381&ev=PageView&noscript=1"
-                      /></noscript>
-                    <!-- End Meta Pixel Code -->
 `,
         }}
       />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src="https://www.facebook.com/tr?id=611716307821381&ev=PageView&noscript=1"
+        />
+      </noscript>
 
       <body className={workSansFont.className}>
         <QueryClientProvider>
